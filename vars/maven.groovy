ffa8a70c3c405fb819268206d3771bd8e1bd4661
Saleemullahpasha <81231909+Saleemullahpasha@users.noreply.github.com>
2022-10-03 17:11:37 +0500

    Update maven.groovy

--- vars/maven.groovy
+++ vars/maven.groovy
@@ -9,8 +9,8 @@ pipeline {
        stages {
            stage("Tools initialization") {
                steps {
-                  sh "mvn --version"
-                  sh "java -version"
+                  mvn --version
+                  java -version
                }
            }
            stage("Checkout Code") {
@@ -21,19 +21,19 @@ pipeline {
            }
            stage("Cleaning workspace") {
                steps {
-                   sh "mvn clean"
+                   mvn clean
                }
            }
            stage("Running Testcase") {
               steps {
-                   sh "mvn test"
+                   mvn test
                }
            }
            stage("Packing Application") {
                steps {
-                   sh "mvn package -DskipTests"
+                   mvn package -DskipTests
                }
            }
        }
- }
+   }
 }
