d15a098e9e57811732fe185fc073e354dbe5c543
Saleemullahpasha <81231909+Saleemullahpasha@users.noreply.github.com>
2022-10-05 12:44:46 +0500

    Update checkout.groovy

--- vars/checkout.groovy
+++ vars/checkout.groovy
@@ -1,17 +1,4 @@
-def call(String GITURL) {
-
-node {
-  env.NAMEMVC="${JOB_BASE_NAME}"
-  env.NAMESPACE="integration"
-  env.IMAGENAME="registry.local/msa/${JOB_BASE_NAME}"
-  env.IMAGETAG="${BUILD_NUMBER}"
-  env.WEB_LOCATION="$WORKSPACE/kubernetes/${JOB_BASE_NAME}"
-  env.KUBECONFIG="$WORKSPACE/kubernetes/kubeconfig/staging"
-  env.GITURL="https://github.com/Saleemullahpasha/tomcat-application.git"
-  env.GIT_DIR_NAME="balance"
-
-
-stage('Code Checkout'){
+def call() {
 
   cleanWs()
   git branch: '${GIT_BRANCH}', credentialsId: 'gitrepo', url: "$GITURL"
@@ -20,7 +7,4 @@ stage('Code Checkout'){
   //IMAGETAG= readFile('.git/commit-id').trim()
   sh 'echo $IMAGETAG'
   echo IMAGETAG
-
-    }
-  }
 }
