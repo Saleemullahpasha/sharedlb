6bec2ec62020574bd8408a84b26a61938dddff62
Saleemullahpasha <81231909+Saleemullahpasha@users.noreply.github.com>
2022-10-05 17:47:25 +0500

    Update checkout.groovy

--- vars/checkout.groovy
+++ vars/checkout.groovy
@@ -1,7 +1,10 @@
 def call(String GITURL) {
 
+  node {
+    stage {
   cleanWs()
   git branch: 'main', credentialsId: 'gitrepo', url: "$GITURL"
   sh "git rev-parse --short HEAD > .git/commit-id"
-  
+    }
+  }
 }
