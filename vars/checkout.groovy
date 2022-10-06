40952c0399c3f65a68ecbe61daea805e0c408ded
Saleemullahpasha <81231909+Saleemullahpasha@users.noreply.github.com>
2022-10-06 10:18:29 +0500

    Update checkout.groovy

--- vars/checkout.groovy
+++ vars/checkout.groovy
@@ -6,11 +6,11 @@ def call(String GITURL) {
       git branch: 'main', url: "$GITURL"
     }
     stage ('Compile') {
-      def javatool = tool name: 'java-11', type: 'jdk'
-       withEnv( ["PATH+JAVATOOL=${javatool}/bin"] ) {
-        def maventool = tool name: 'maven', type: 'maven'
-        withEnv( ["PATH+MAVENTOOL=${maventool}/bin"] ) {
-        bat 'mvn clean install -DskipTests'
+        
+      bat "mvn clean"
+      bat "mvn install"
+      bat "mvn package -DskipTests"
+      
         }
     }
   }
