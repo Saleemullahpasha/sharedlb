3bde6f11262354a5dec5502b33f2abbe3ef8ca13
Saleemullahpasha <81231909+Saleemullahpasha@users.noreply.github.com>
2022-10-06 10:25:52 +0500

    Update checkout.groovy

--- vars/checkout.groovy
+++ vars/checkout.groovy
@@ -5,11 +5,21 @@ def call(String GITURL) {
       cleanWs()
       git branch: 'main', url: "$GITURL"
     }
+    
     stage ('Compile') {
-        
       bat "mvn clean"
       bat "mvn install"
       bat "mvn package -DskipTests"
+        }
+    stage ('Scan-code') {
+  
+      sh 'sonar-scanner  ' +
+      '-Dsonar.projectKey=$JOB_BASE_NAME ' +
+      '-Dsonar.projectName=$JOB_BASE_NAME ' +
+      '-Dsonar.projectVersion=1.0 ' +
+      '-Dsonar.sources=$WORKSPACE/src ' +
+      '-Dsonar.binaries=$WORKSPACE/ ' +
+      '-Dsonar.java.binaries=$WORKSPACE/ '
       
         }
     }
